06409f2341f54a6820b8f371d6084209bc7f6153
AvailableName1 <liker1011@gmail.com>
2022-08-05 20:24:36 +0400

    Ready for testing. Basics learnt.

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,6 +6,7 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    uint private seed;
     address[] whoInteracted;
 
     event NewWave(address indexed from, uint256 timestamp, string message);
@@ -18,15 +19,39 @@ contract WavePortal {
 
     Wave[] waves;
 
-    constructor() {
+    mapping (address => uint256) public lastWavedAt;
+
+    constructor() payable {
         console.log('torbik has been here');
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
+        require(
+            lastWavedAt[msg.sender] + 1 minutes < block.timestamp,
+            'wait 1 min please'
+        );
+
         totalWaves += 1;
         whoInteracted.push(msg.sender);
         console.log('%s had waved w/ message is %s', msg.sender, _message);
         waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        console.log('seed is %d', seed);
+
+        if (seed < 30) {
+            uint256 prizeAmount;
+            prizeAmount = 0.0001 ether;
+            console.log('%s won!', msg.sender);
+            require(
+            prizeAmount <= address(this).balance, 'failed bc not enough money'
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}('');
+            require(success, "Failed to withdraw money from contract");
+        }
+
+        lastWavedAt[msg.sender] = block.timestamp;
 
         emit NewWave(msg.sender, block.timestamp, _message);
     }
--- scripts/run.js
+++ scripts/run.js
@@ -1,13 +1,23 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy();
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
   await waveContract.deployed();
   console.log("Contract deployed to:", waveContract.address);
   console.log("Contract deployed by:", owner.address);
 
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
+
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "contract balance",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
   let waveTxn;
   waveTxn = await waveContract.wave("someMessage");
@@ -17,6 +27,15 @@ const main = async () => {
 
   waveTxn = await waveContract.connect(randomPerson).wave("I am a messsssage");
   await waveTxn.wait();
+
+  waveTxn = await waveContract.connect(randomPerson).wave("I am a messsssage");
+  await waveTxn.wait();
+
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Now contract balance",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
   waveCount = await waveContract.getTotalWaves();
 
